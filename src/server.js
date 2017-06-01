e95d7628c1ecb92abbb002e95397790de243979f
Jonathan M. Hardison <jmh@jonathanhardison.com>
2017-05-31 21:41:14 -0500

    Initial structure

---
+++ src/server.js
@@ -0,0 +1,10 @@
+////////////////////////////////////////
+// ADFSUserMappingAPI
+//
+// Copyright (c) 2017 Jonathan Hardison
+////////////////////////////////////////
+
+import http from 'http';
+import express from 'express';
+import mongoose from 'mongoose';
+
