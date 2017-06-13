598420ea7d1044278e2f177a840e8863a5c11075
Jonathan Hardison <jmh@jonathanhardison.com>
2017-06-13 13:21:08 -0500

    change 1

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.server = http.createServer(app);
 
 // middleware //
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'sts.simd.today');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
